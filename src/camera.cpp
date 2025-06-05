9f159d42e84369f3333f876b42ef805daa7890e8
miles632 <asphyxia123@protonmail.com>
2025-06-05 22:35:43 +0200

    updated camera

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,5 +1,7 @@
 #include "camera.h"
 
+#include <glm/ext/matrix_transform.hpp>
+
 void Camera::move(float dT) {
     if (keys[GLFW_KEY_W])
         pos += speed * front * dT;
@@ -9,19 +11,40 @@ void Camera::move(float dT) {
         pos -= glm::normalize(glm::cross(front, up)) * speed * dT;
     if (keys[GLFW_KEY_D])
         pos += glm::normalize(glm::cross(front, up)) * speed * dT;
-}
 
-void Camera::updateFront() {
-
+    if (keys[GLFW_KEY_SPACE])
+        pos += up * speed * dT;
+    if (keys[GLFW_KEY_LEFT_SHIFT])
+        pos -= up * speed * dT;
 }
 
 glm::mat4 Camera::getViewMatrix() const {
-
+    return glm::lookAt(pos, pos + front, up);
 }
 
 void Camera::mouse(float xOffset, float yOffset, float sensitivity) {
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
 
+    yaw += xOffset;
+    pitch -= yOffset;
+
+    // clamp
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (yaw > 89.0f)
+        yaw = 89.0f;
+
+    // recompute the front vector
+    updateFront();
 }
 
+void Camera::updateFront() {
+    glm::vec3 dir;
 
+    dir.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    dir.y = sin(glm::radians(pitch));
+    dir.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
 
+    glm::normalize(dir);
+}
